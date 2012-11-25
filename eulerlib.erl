c6c53e5273601bd6ec1893a10ded8bfc451fa89d
Rasmus Mattsson <rasmus.m.mattsson@gmail.com>
2012-11-25 09:59:25 +0100

    Moved some functions into eulerlib.

--- eulerlib.erl
+++ eulerlib.erl
@@ -1,7 +1,25 @@
 -module (eulerlib).
 
--export ([execution_time/2]).
+-export ([parse_digits/1, fac/1]).
 
-execution_time_sec({StartMSec, StartSec, _}, {EndMSec, EndSec, _}) ->
-	(EndMSec - StartMSec) + (EndSec - StartMSec). 
+%% ----------------------------------
+%% @doc Parses a base 10 number into its individual digits.
+%% Returns all the digits of the number that was passed as an argument
+%% as an array of (integer) digits.
+%% @end
+%% ----------------------------------
+parse_digits(Number) ->
+	StringList = integer_to_list(Number),
+	[list_to_integer([X]) || X <- StringList].
 
+%% ----------------------------------
+%% @doc Calculates the faculty of the number passed as an argument. 
+%% @end
+%% ----------------------------------
+fac(Number) ->
+	fac_tr(Number, 1).
+
+fac_tr(0, Acc) ->
+	Acc;
+fac_tr(Number, Acc) ->
+	fac_tr(Number - 1, Acc * Number).
