a137972a560f1aae942f7cf200f7dc7b7f3e107a
Rasmus Mattsson <rasmus.m.mattsson@gmail.com>
2012-11-25 10:00:20 +0100

    Added solution to problem20 as file euler20.erl

---
+++ euler20.erl
@@ -0,0 +1,12 @@
+#!/usr/bin/env escript
+
+% n! means n × (n − 1) × ... × 3 × 2 × 1
+% For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+% and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+% Find the sum of the digits in the number 100!
+
+main(_) ->
+	Number  = eulerlib:fac(100),
+	Digits  = eulerlib:parse_digits(Number),
+	Sum     = lists:sum(Digits),
+	io:format("Answer: ~w~n", [Sum]).
