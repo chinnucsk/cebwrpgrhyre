9d12850db2cda5bf3975643505c06afa32530564
Rasmus Mattsson <rasmus.m.mattsson@gmail.com>
2012-11-25 22:15:45 +0100

    Added function to find all the proper divisors of a number.

--- eulerlib.erl
+++ eulerlib.erl
@@ -1,6 +1,6 @@
 -module (eulerlib).
 
--export ([parse_digits/1, fac/1]).
+-export ([parse_digits/1, fac/1, proper_divisors/1]).
 
 %% ----------------------------------
 %% @doc Parses a base 10 number into its individual digits.
@@ -23,3 +23,21 @@ fac_tr(0, Acc) ->
 	Acc;
 fac_tr(Number, Acc) ->
 	fac_tr(Number - 1, Acc * Number).
+
+%% ----------------------------------
+%% @doc Returns all the proper divisors of a number.
+%% @end
+%% ----------------------------------
+proper_divisors(Number) ->
+	proper_divisors(Number, 1, []).
+
+proper_divisors(Number, Candidate, Acc) when Candidate > Number div 2 ->
+	Acc;
+proper_divisors(Number, Candidate, Acc) ->
+	Acc2 = case Number rem Candidate of 
+		0 ->
+      [Candidate|Acc];
+    _ ->
+      Acc
+  end,
+  proper_divisors(Number, Candidate + 1, Acc2).
