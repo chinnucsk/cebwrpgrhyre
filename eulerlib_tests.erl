eab8a473437737ca131548554814d2d72d397cca
Rasmus Mattsson <rasmus.m.mattsson@gmail.com>
2012-12-02 18:14:58 +0100

    Added test for proper_divisors() in eulerlib.

--- eulerlib_tests.erl
+++ eulerlib_tests.erl
@@ -2,6 +2,13 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-compile(export_all).
+
+proper_divisors_test() ->
+  Expected = [14, 7, 4, 2, 1],
+  Actual   = eulerlib:proper_divisors(28),
+  ?assertMatch([14, 7, 4, 2, 1], Actual).
+
 mapreduce_test() ->
 	A       = [6,5],
   B       = [5,4],
@@ -19,9 +26,3 @@ mapreduce_test() ->
             end,
   Answer  = eulerlib:mapreduce(Mapper, Reducer, Data, []),  
   ?assertMatch([1,2,3,4,5,6], Answer).
-
-invert_list_test() ->
-  InvertMe = [1,3,5,7,10],
-  Expected = [2,4,6,8,9],
-  Actual   = eulerlib:invert_list(InvertMe),
-  ?assertMatch(Expected,Actual).
