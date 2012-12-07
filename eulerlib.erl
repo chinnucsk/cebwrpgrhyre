0edd5ef25b452d032bc17e1806f5b82e9bf42b43
Rasmus Mattsson <rasmus.m.mattsson@gmail.com>
2012-12-07 22:37:46 +0100

    Added -spec tags to eulerlib.erl

--- eulerlib.erl
+++ eulerlib.erl
@@ -1,3 +1,16 @@
+%% @author Rasmus Mattsson <[rasmus.m.mattsson] [at] [gmail] [dot] [com]>
+
+%% ----------------------------------
+%% @doc Support library for solving problems from project-euler.com 
+%% in Erlang.<br/>
+%% This library contains conveniance functions that can be reused
+%% in-between problems. The praxis is that when a function is used by more 
+%% than one problem it is placed in this file.
+%% This library is also a victim of me trying out edoc for the first time. 
+%% Just in case you wondered why the level of documentation is far better than
+%% in all any other source file that I leave behind ;)
+%% @end
+%% ----------------------------------
 -module (eulerlib).
 
 -include ("eulerlib.hrl").
@@ -9,11 +22,24 @@
           abs/1]).
 
 %% ----------------------------------
-%% @doc Parses a base 10 number into its individual digits.
-%% Returns all the digits of the number that was passed as an argument
-%% as an array of (integer) digits.
+%% @doc Returns the absolute value of a number.
 %% @end
 %% ----------------------------------
+-spec abs(Num :: integer()) -> integer().
+
+abs(Num) when Num < 0 -> 
+  -Num;
+abs(Num) ->
+  Num.
+
+%% ----------------------------------
+%% @doc Parses a base-10 number into digits. 
+%% The digits are returned in a 'big-endian' list. 
+%% @see numlist_to_integer
+%% @end
+%% ----------------------------------
+-spec parse_digits(Number :: integer()) -> list(integer()).
+
 parse_digits(Number) ->
 	StringList = integer_to_list(Number),
 	[list_to_integer([X]) || X <- StringList].
@@ -21,9 +47,12 @@ parse_digits(Number) ->
 %% ----------------------------------
 %% @doc Interprets a list as a base-10 integer number.
 %% List is interpreted in a big-endian sense, and each position in the 
-%% list is interpreted as a power of 10 lower than the previous one (=the position to the left).
+%% list is interpreted as a power of 10 lower than the previous one.
+%% @see parse_digits
 %% @end
 %% ----------------------------------
+-spec numlist_to_integer(List :: list(integer())) -> integer().
+
 numlist_to_integer(List) ->
   numlist_to_integer(List, 0).
 
@@ -38,6 +67,8 @@ numlist_to_integer([H|T] = List, Acc) ->
 %% @doc Counts the number of digits in a base-10 number.
 %% @end
 %% ----------------------------------
+-spec count_digits(Number :: integer()) -> integer().
+
 count_digits(Number) ->
   count_digits(Number, 0).
 
@@ -48,9 +79,11 @@ count_digits(_, Acc) ->
   Acc.
 
 %% ----------------------------------
-%% @doc Calculates the faculty of the number passed as an argument. 
+%% @doc Calculates the faculty of a number.
 %% @end
 %% ----------------------------------
+-spec fac(Number :: integer()) -> integer().
+
 fac(Number) ->
 	fac_tr(Number, 1).
 
@@ -63,6 +96,7 @@ fac_tr(Number, Acc) ->
 %% @doc Iterate thorugh all fibonacci numbers, c-style. Returns an updated state with the newest number as next.
 %% @end
 %% ----------------------------------
+
 fib_iter(S) ->
   #fibstate{first  = S#fibstate.first + S#fibstate.second,
             second = S#fibstate.first,
@@ -72,6 +106,8 @@ fib_iter(S) ->
 %% @doc Returns all the proper divisors of a number.
 %% @end
 %% ----------------------------------
+-spec proper_divisors(Number :: integer()) -> list(integer()).
+
 proper_divisors(Number) ->
 	proper_divisors(Number, 1, []).
 
@@ -91,6 +127,8 @@ proper_divisors(Number, Candidate, Acc) ->
 %% Number that is tested must be a true integer, e.g 123 (123.0 is not ok).
 %% @end
 %% ----------------------------------
+-spec is_prime(Number :: integer()) -> boolean().
+
 is_prime(2) ->
   true;
 is_prime(Number) when Number rem 2 /= 0 ->
@@ -108,8 +146,10 @@ is_prime_tr(Number, Divisor, MaxTestVal) ->
 %% ----------------------------------
 %% @doc Returns the maximum of Num1 and Num2.
 %% If both numbers are equal Num1 is returned.
+%% @deprecated use erlang:max instead.
 %% @end
 %% ----------------------------------
+
 max(Num1, Num2) ->
   if
     Num1 >= Num2 ->
@@ -121,6 +161,7 @@ max(Num1, Num2) ->
 %% ----------------------------------
 %% @doc Returns the minimum of Num1 and Num2.
 %% If both numbers are equal Num1 is returned.
+%% @deprecated use erlang:min instead.
 %% @end
 %% ----------------------------------
 min(Num1, Num2) ->
@@ -139,6 +180,9 @@ min(Num1, Num2) ->
 %% in the file.
 %% @end
 %% ----------------------------------
+-spec parse_int_problem_file(Filename :: atom() | string()) 
+                                      -> list(list(any())).
+
 parse_int_problem_file(Filename) ->
   {ok, IoDevice} = file:open(Filename, [read]),
   parse_int_problem_file(IoDevice, []).
@@ -158,26 +202,37 @@ parse_int_problem_file(IoDevice, Acc) ->
   end. 
 
 %% ----------------------------------
-%% @doc A simple map-reduce implementation.
-%% MapFun    => Function that does the mapping
-%% ReduceFun => Function that does the reduction
-%% Data      => The data that the mappers should work on. 
+%% @doc A simple map-reduce implementation.<br/>
+%% Mapper    => Function that does the mapping. Each mapper will be spawned 
+%%              as a separate process. The Mapper takes a value of any type and 
+%%              returns a result that will eventually be passed to the reducer.
+%%              <br/>
+%% Reducer   => Function that does the reduction. Will be spawned as one 
+%%              process. Takes a result from the mapper along with an 
+%%              accumulator, returns the new accumulator.
+%%              <br/>
+%% Data      => The data that each mapper should work on. 
 %%              One mapper will be spawned for each term in this list.
-%% Acc0      => Initial accumulator that will be passed to the reducer  
+%%              <br/>
+%% Acc0      => Initial accumulator that will be passed to the reducer, along
+%%              with the response from the first mapper.  
 %% @end
 %% ----------------------------------
-mapreduce(MapFun, ReduceFun, Data, Acc0) ->
-  SelfPid = self(),
-  io:format("Processes before map-reduce spawn: ~w~n", [length(processes())]),
-  ReducerPid = spawn(fun () -> reducer(SelfPid, ReduceFun, length(Data), Acc0) end),
+-spec mapreduce(Mapper    :: fun((any()) -> any()), 
+                Reducer   :: fun((Result :: any(), Acc :: any()) 
+                          -> Acc2 :: any()),
+                Data      :: list(any()),
+                Acc0      :: any()) 
+                          -> AccN :: any().
+
+mapreduce(Mapper, Reducer, Data, Acc0) ->
+  SelfPid    = self(),
+  ReducerPid = spawn(fun () -> 
+                       reducer(SelfPid, Reducer, length(Data), Acc0)
+                     end),
   lists:foreach(fun (X) ->
-                  spawn(fun () -> mapper(ReducerPid, MapFun, X) end)
+                  spawn(fun () -> mapper(ReducerPid, Mapper, X) end)
                 end, Data),
-  io:format("Processes after map-reduce spawn: ~w~n", [length(processes())]),
-  timer:sleep(1000),
-  io:format("Processes 1 sec map-reduce spawn: ~w~n", [length(processes())]),
-  timer:sleep(1000),
-  io:format("Processes 2 sec map-reduce spawn: ~w~n", [length(processes())]),
   receive  
     {ReducerPid, Result} ->
       Result;
@@ -185,14 +240,14 @@ mapreduce(MapFun, ReduceFun, Data, Acc0) ->
       Error
   end.
 
-reducer(ParentPid, ReduceFun, Remaining, Acc) ->
+reducer(ParentPid, Reducer, Remaining, Acc) ->
   receive
     {mapper_done, Result} ->
       Remaining2 = Remaining - 1,
-      Acc2 = ReduceFun(Result, Acc),
+      Acc2       = Reducer(Result, Acc),
       case Remaining2 > 0 of
         true ->
-          reducer(ParentPid, ReduceFun, Remaining2, Acc2);
+          reducer(ParentPid, Reducer, Remaining2, Acc2);
         false ->
           ParentPid ! {self(), Acc2}
       end;
@@ -204,8 +259,3 @@ mapper(ReducerPid, MapperFun, X) ->
   Result = MapperFun(X),
   ReducerPid ! {mapper_done, Result}.
 %% ----------------------------------
-
-abs(Num) when Num < 0 -> 
-  -Num;
-abs(Num) ->
-  Num.
